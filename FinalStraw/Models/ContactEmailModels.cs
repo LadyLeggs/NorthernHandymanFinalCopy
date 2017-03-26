9fc2c15753c1aba1c779b57032ebb7d76443b769
Authorty <itismejody@GMAIL.COM>
2017-03-26 10:51:44 -0500

    Changed contact page

---
+++ FinalStraw/Models/ContactEmailModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheNorthernHandyman.Models
+{
+    public class ContactEmailModels
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+    }
+}
